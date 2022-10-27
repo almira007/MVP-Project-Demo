34df58ee6d48551e4c1f21a44bbf2b793bd8e286
Almira Shaikh <almira.shaikh@1Rintern.local>
2022-10-27 15:56:07 +0530

    feat: add

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -6,9 +6,7 @@ import { UserComponent } from './user.component';
 import { UserFormContainerComponent } from './user-form-container/user-form-container.component';
 import { UserListContainerComponent } from './user-list-container/user-list-container.component';
 import { UserFormPresentationComponent } from './user-form-container/user-form-presentation/user-form-presentation.component';
-import { UserFormPresenterComponent } from './user-form-container/user-form-presenter/user-form-presenter.component';
 import { UserListPresentationComponent } from './user-list-container/user-list-presentation/user-list-presentation.component';
-import { UserListPresenterComponent } from './user-list-container/user-list-presenter/user-list-presenter.component';
 
 
 @NgModule({
@@ -17,9 +15,7 @@ import { UserListPresenterComponent } from './user-list-container/user-list-pres
     UserFormContainerComponent,
     UserListContainerComponent,
     UserFormPresentationComponent,
-    UserFormPresenterComponent,
     UserListPresentationComponent,
-    UserListPresenterComponent
   ],
   imports: [
     CommonModule,
